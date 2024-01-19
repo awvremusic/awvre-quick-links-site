9a69f92de00c4c699bea369a67416fb5bb91132a
Bryce DeVaughn <awvremusic@gmail.com>
2024-01-19 10:49:41 -0600

    Fixed issue with types

--- src/features/quickLinksPage/businessObjects/QuickLinksPage/QuickLinksPage.ts
+++ src/features/quickLinksPage/businessObjects/QuickLinksPage/QuickLinksPage.ts
@@ -1,21 +1,16 @@
 export class QuickLinksPage {
-    private readonly id: string;
     private readonly heading: string;
-    private readonly displayLive: boolean;
-    private readonly versionId: string;
     private readonly mediaLinks: string[];
     private readonly latestRelease: string;
-    private readonly profileImage: HygraphImage;
+    private readonly profileImage: {
+        id: string,
+        url: string,
+    };
     private readonly description: string;
-    private readonly updatedAt: string;
-    private readonly publishedAt: string;
 
     constructor(data: Partial<QuickLinksPageData>) {
         const {
-            id = "",
             heading = "",
-            displayLive = false,
-            versionId = "",
             mediaLinks = [],
             latestRelease = "",
             profileImage = {
@@ -23,36 +18,17 @@ export class QuickLinksPage {
                 url: "",
             },
             description = "",
-            updatedAt = "",
-            publishedAt = "",
         } = data;
 
-        this.id = id;
         this.heading = heading;
-        this.displayLive = displayLive;
-        this.versionId = versionId;
         this.mediaLinks = mediaLinks.map((mediaLink: { id: string }) => mediaLink.id);
         this.latestRelease = latestRelease;
         this.profileImage = profileImage;
         this.description = description;
-        this.updatedAt = updatedAt;
-        this.publishedAt = publishedAt;
-    }
-
-    public getId(): string {
-        return this.id;
     }
 
     public getHeading(): string {
         return this.heading;
-    }
-
-    public getDisplayLive(): boolean {
-        return this.displayLive;
-    }
-
-    public getVersionId(): string {
-        return this.versionId;
     }
 
     public getMediaLinks(): string[] {
@@ -73,21 +49,5 @@ export class QuickLinksPage {
 
     public getDescription(): string {
         return this.description;
-    }
-
-    public getUpdatedAt(): string {
-        return this.updatedAt;
-    }
-
-    public getPublishedAt(): string {
-        return this.publishedAt;
-    }
-
-    public isOutdated(): boolean {
-        if (this.publishedAt === "") {
-            return false;
-        }
-
-        return this.updatedAt > this.publishedAt;
     }
 }
